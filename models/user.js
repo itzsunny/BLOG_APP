7f2bf6301aa557893f6caa604492087f7e7ff517
sunny <sunnychoudhury007@gmail.com>
2019-11-05 21:24:05 +0530

    update ui

--- models/user.js
+++ models/user.js
@@ -1,20 +1,42 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
-const userSchema = new Schema({
-    author_name:{
-        type:String,
-        required:true
+const userSchema = new Schema(
+  {
+    author_name: {
+      type: String,
+      required: true
     },
-    author_introduction:{
-        type:String,
+    author_introduction: {
+      type: String,
+      required: true
     },
-    profile_pic:{
-        type:String
+    profile_pic: {
+      type: String
     },
-    article_title:{
-        type:String,
-        
+    article_title: {
+      type: String,
+      required: true
+    },
+    article_intro: {
+      type: String
+    },
+    article_subtitle: {
+      type: String
+    },
+    article_snippets:{
+      type:String
+    },
+    article_description: {
+      type: String
+    },
+    likes: {
+      type:Number,
+      default:0
     }
-})
+  },
+  { timestamps: true }
+);
+
+
+module.exports = mongoose.model("User",userSchema);
