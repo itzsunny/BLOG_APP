049ebec3d60a4ae9a15ee27ce3c3b5fdecb2a301
sunny <sunnychoudhury007@gmail.com>
2019-11-11 11:54:46 +0530

    update Procfile

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,4 @@
+
 function readURL(input) {
   if (input.files && input.files[0]) {
     var reader = new FileReader();
@@ -9,32 +10,35 @@ function readURL(input) {
     reader.readAsDataURL(input.files[0]);
   }
 }
+
 var comments = document.querySelector("#view_all_comments");
 var comment = document.querySelector(".view_comments");
 console.log(comments)
 
-comment.addEventListener("click",()=> {
-  if(comments.checked){
-    comment.innerText = "View all";
-  } else {
-    comment.innerText = "View less";
-  }
-})
-var commentBtn = document.getElementById("update_btn");
+if(comment) {
+  comment.addEventListener("click",()=> {
+    if(comments.checked){
+      comment.innerText = "View all";
+    } else {
+      comment.innerText = "View less";
+    }
+  });
+}
+
+var commentBtn = document.querySelector("#update_btn");
 console.log(commentBtn)
 
 var snippet = document.querySelector(".label_snippet");
 console.log(snippet)
 
+if(snippet){
+  snippet.addEventListener("click",()=> {
+    snippet.style.backgroundColor = "red";
+  })
+}
+if(commentBtn){
+  commentBtn.addEventListener("click",()=> {
+    commentBtn.style.transform = "rotate(360deg)";
+})
+}
 
-// commentBtn.addEventListener("mouseover",()=> {
-//   commentBtn.style.backgroundColor = "red";
-// })
-// var snippet = document.querySelector(".article_snippets").Value;
-// var snippets = document.querySelector(".snippet");
-// console.log(snippet);
-// if (snippet == undefined) {
-//   snippets.style.display = "none";
-// } else {
-//   snippets.style.display = "block";
-// }
